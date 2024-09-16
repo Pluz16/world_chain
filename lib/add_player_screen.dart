5bc56be503235f04065e67d30abc31f316cadb32
Francesco Punta <puntafrancesco@gmail.com>
2024-09-16 14:21:50 +0200

    Modifica codice di Add Player

--- lib/add_player_screen.dart
+++ lib/add_player_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'select_country_screen.dart'; // Importiamo la schermata per selezionare il paese
+import 'europe_map_screen.dart'; // Assicurati di importare la mappa
 
 class AddPlayerScreen extends StatefulWidget {
-  const AddPlayerScreen({super.key});
+  const AddPlayerScreen({Key? key}) : super(key: key);
 
   @override
   _AddPlayerScreenState createState() => _AddPlayerScreenState();
@@ -23,17 +23,19 @@ class _AddPlayerScreenState extends State<AddPlayerScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            // Input per il nome del giocatore
             TextField(
               controller: _nameController,
               decoration: const InputDecoration(labelText: 'Nome del Giocatore'),
             ),
             const SizedBox(height: 20),
+            // Pulsante per selezionare il paese dalla mappa
             ElevatedButton(
               onPressed: () async {
                 final country = await Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => const SelectCountryScreen(),
+                    builder: (context) => const EuropeMapScreen(),
                   ),
                 );
                 if (country != null) {
@@ -45,9 +47,11 @@ class _AddPlayerScreenState extends State<AddPlayerScreen> {
               child: Text(selectedCountry == null ? 'Seleziona Stato' : 'Stato: $selectedCountry'),
             ),
             const SizedBox(height: 20),
+            // Pulsante per aggiungere il giocatore
             ElevatedButton(
               onPressed: () {
                 if (_nameController.text.isNotEmpty && selectedCountry != null) {
+                  // Ritorna il nome e il paese selezionato
                   Navigator.pop(context, {
                     'name': _nameController.text,
                     'country': selectedCountry,
