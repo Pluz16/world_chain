ecc16ec99cd8952a4fdce23d54eab7fe33b02af5
Francesco Punta <puntafrancesco@gmail.com>
2024-09-17 15:23:37 +0200

    Cambio logica selezione starting point. Geojson.io fornisce i dati. Adesso bisogna collegare id a On tap

--- lib/europe_map_screen.dart
+++ lib/europe_map_screen.dart
@@ -6,7 +6,7 @@ import 'dart:convert';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 
 class GeoJsonMap extends StatefulWidget {
-  const GeoJsonMap({super.key});
+  const GeoJsonMap({Key? key}) : super(key: key);
 
   @override
   _GeoJsonMapState createState() => _GeoJsonMapState();
@@ -14,7 +14,8 @@ class GeoJsonMap extends StatefulWidget {
 
 class _GeoJsonMapState extends State<GeoJsonMap> {
   final MapController _mapController = MapController();
-  List<Polygon> polygons = [];
+  List<Marker> markers = [];
+  List<Polyline> polylines = [];
   bool isLoading = true;
   String? errorMessage;
 
@@ -26,42 +27,48 @@ class _GeoJsonMapState extends State<GeoJsonMap> {
 
   Future<void> loadGeoJson() async {
     try {
-      final geoJsonData = await rootBundle.loadString('assets/europe.geojson');
+      final geoJsonData = await rootBundle.loadString('assets/cities.geojson');
       final data = jsonDecode(geoJsonData);
 
-      List<Polygon> loadedPolygons = [];  // Raccogli i poligoni in una lista temporanea
+      List<Marker> loadedMarkers = [];
+      List<Polyline> loadedPolylines = [];
 
       for (var feature in data['features']) {
-        if (feature['geometry']['type'] == 'Polygon' || feature['geometry']['type'] == 'MultiPolygon') {
-          List<LatLng> points = [];
+        if (feature['geometry']['type'] == 'Point') {
+          var coordinates = feature['geometry']['coordinates'];
+          LatLng point = LatLng(coordinates[1], coordinates[0]);
 
-          if (feature['geometry']['type'] == 'Polygon') {
-            for (var coord in feature['geometry']['coordinates'][0]) {
-              points.add(LatLng(coord[1], coord[0]));
-            }
-          } else if (feature['geometry']['type'] == 'MultiPolygon') {
-            for (var polygon in feature['geometry']['coordinates']) {
-              for (var coord in polygon[0]) {
-                points.add(LatLng(coord[1], coord[0]));
-              }
-            }
+          loadedMarkers.add(
+            Marker(
+              point: point,
+              width: 30.0, // Dimensione icona
+              height: 30.0,
+              child: const Icon(
+                Icons.location_on,
+                color: Colors.red,
+                size: 30,
+              ),
+            ),
+          );
+        } else if (feature['geometry']['type'] == 'LineString') {
+          List<LatLng> points = [];
+          for (var coord in feature['geometry']['coordinates']) {
+            points.add(LatLng(coord[1], coord[0]));
           }
 
-          // Aggiungi il poligono alla lista
-          loadedPolygons.add(
-            Polygon(
+          loadedPolylines.add(
+            Polyline(
               points: points,
-              color: Colors.blue.withOpacity(0.3),
-              borderStrokeWidth: 2,
-              borderColor: Colors.blue,
+              color: Colors.blue,
+              strokeWidth: 3.0,
             ),
           );
         }
       }
 
-      // Aggiorna lo stato una volta sola
       setState(() {
-        polygons = loadedPolygons;
+        markers = loadedMarkers;
+        polylines = loadedPolylines;
         isLoading = false;
       });
     } catch (e) {
@@ -79,22 +86,30 @@ class _GeoJsonMapState extends State<GeoJsonMap> {
         title: const Text('Mappa GeoJSON dell\'Europa'),
       ),
       body: isLoading
-          ? const Center(child: CircularProgressIndicator())  // Mostra un indicatore di caricamento
+          ? const Center(child: CircularProgressIndicator())
           : errorMessage != null
-              ? Center(child: Text(errorMessage!))  // Mostra un messaggio di errore se c'è
+              ? Center(child: Text(errorMessage!))
               : FlutterMap(
                   mapController: _mapController,
                   options: MapOptions(
-                    onMapReady: () {
-                      _mapController.move(LatLng(51.5, 10.0), 5.0);  // Imposta centro e zoom
+                    initialCenter: LatLng(51.5, 10.0), // Centro Europa
+                    initialZoom: 5.0, // Zoom iniziale
+                    onTap: (tapPosition, latlng) {
+                      print('Hai cliccato su: $latlng');
                     },
                   ),
                   children: [
                     TileLayer(
-                      urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                      tileProvider: CancellableNetworkTileProvider(),  // Usa il provider ottimizzato
+                      urlTemplate:
+                          'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                      tileProvider: CancellableNetworkTileProvider(),
                     ),
-                    PolygonLayer(polygons: polygons),
+                    MarkerLayer(
+                      markers: markers,
+                    ),
+                    PolylineLayer(
+                      polylines: polylines,
+                    ),
                   ],
                 ),
     );
